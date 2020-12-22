63b9a716db4d8d05e9ea399f98a64dc948370196
Jason Beedle <jbeedle@gmail.com>
2020-12-22 15:06:45 +0000

    initial commit

---
+++ beinsportsmaxfour.py
@@ -0,0 +1,33 @@
+import datetime
+import csv
+import pendulum
+import requests
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
+
+api_url = "https://secure-webtv-static.canal-plus.com/metadata/cpfra/all/v2.2/globalchannels.json"
+response = requests.get(api_url).json()
+
+tv_programme = {
+    channel["name"]: [
+        [
+            e['title'],
+            e['subTitle'],
+            pendulum.parse(e['timecodes'][0]['start']
+                           ).time().strftime("%H:%M"),
+        ] for e in channel["events"]
+    ] for channel in response["channels"]
+}
+
+
+print(tabulate(
+    tv_programme["BEIN SPORTS MAX 4"],
+    headers=["Title", "Subtitle", "Time", "Duration"],
+    tablefmt="csv",
+))
+df = pd.DataFrame(tv_programme["BEIN SPORTS MAX 4"],
+                  columns=["Title", "Subtitle", "Time", ])
+df.to_csv("bein_sports_max_four.csv", index=False)
+
+# print("\n".join(sorted(list(tv_programme.keys()))))
---
+++ canalPlusSport.py
@@ -0,0 +1,33 @@
+import datetime
+import csv
+import pendulum
+import requests
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
+
+api_url = "https://secure-webtv-static.canal-plus.com/metadata/cpfra/all/v2.2/globalchannels.json"
+response = requests.get(api_url).json()
+
+tv_programme = {
+    channel["name"]: [
+        [
+            e['title'],
+            e['subTitle'],
+            pendulum.parse(e['timecodes'][0]['start']
+                           ).time().strftime("%H:%M"),
+        ] for e in channel["events"]
+    ] for channel in response["channels"]
+}
+
+
+print(tabulate(
+    tv_programme["CANAL+ SPORT"],
+    headers=["Title", "Subtitle", "Time", "Duration"],
+    tablefmt="csv",
+))
+df = pd.DataFrame(tv_programme["CANAL+ SPORT"],
+                  columns=["Title", "Subtitle", "Time", ])
+df.to_csv("canal_Plus_sports.csv", index=False)
+
+print("\n".join(sorted(list(tv_programme.keys()))))
